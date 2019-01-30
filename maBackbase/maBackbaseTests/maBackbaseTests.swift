8c8b65f5fce59289551d390c1edc2e93547b7f45
Kyryl <kirill.ge@gmail.com>
2019-01-30 22:39:43 +0200

    add files to repo

---
+++ maBackbase/maBackbaseTests/maBackbaseTests.swift
@@ -0,0 +1,33 @@
+//
+//  maBackbaseTests.swift
+//  maBackbaseTests
+//
+//  Created by Kirill Gorbushko on 1/30/19.
+//  Copyright © 2019 - present. All rights reserved.
+//
+
+import XCTest
+@testable import maBackbase
+
+class maBackbaseTests: XCTestCase {
+
+    override func setUp() {
+
+    }
+
+    override func tearDown() {
+
+    }
+
+    func testExample() {
+
+    }
+
+    func testPerformanceExample() {
+
+        self.measure {
+
+        }
+    }
+
+}
